7b2af44b32ccb5841605dd3a6a7390d5c9bc9bba
bapp234 <hoangsang998@gmail.com>
2026-07-11 18:31:59 +0700

    feat(application): configure application layer

---
+++ backend/ProjectHub/src/ProjectHub.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+
+namespace ProjectHub.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
